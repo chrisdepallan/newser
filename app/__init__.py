9924c0ec23b215e5f5277450440276b8004f4297
chris de pallan <70682683+chrisdepallan@users.noreply.github.com>
2024-08-29 22:26:10 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,44 +5,15 @@ from pymongo import MongoClient
 from authlib.integrations.flask_client import OAuth
 from app.utils import NewsAPIClient, datetime_filter
 from flask_mail import Mail
-from flask_cors import CORS
-import redis
 
-# Initialize Flask app
-import redis
+from flask_cors import CORS
 
 # Initialize Flask app
 app = Flask(__name__, template_folder='templates')
 app.config.from_object('config.Config')
 
-# Configure Redis for session management
-redis_host = 'redis-11738.c244.us-east-1-2.ec2.redns.redis-cloud.com'
-redis_port = 11738
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_REDIS'] = redis.Redis(host=redis_host, port=redis_port)
-
 # Initialize extensions
 Session(app)
-app.config.from_object('config.Config')
-
-# Configure Redis for session management
-redis_host = 'redis-11738.c244.us-east-1-2.ec2.redns.redis-cloud.com'
-redis_port = 11738
-redis_password = 'X51UKTJVjgI2727qmgGyEHegLLTkPNSQ'  # Replace with your actual Redis password
-
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_REDIS'] = redis.Redis(
-    host=redis_host,
-    port=redis_port,
-    password=redis_password
-    )
-
-# Initialize extensions
-
 
 bcrypt = Bcrypt(app)
 oauth = OAuth(app)
